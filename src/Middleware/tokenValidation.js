e95c984df6b16f0bc0eff2365a3a82bf3381be78
Emran Atrooz <emranatrooz03@example.com>
2025-10-03 01:06:18 +0300

    Added a reset token validation

---
+++ src/Middleware/tokenValidation.js
@@ -0,0 +1,62 @@
+import { getResetToken } from "../Models/resetPassword.model.js";
+
+export const validateResetToken = async (req, res, next) => {
+    try {
+        const { token } = req.body;
+
+        //token provided? :
+        if (!token) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: "Reset token is required",
+                }
+            );
+        };
+
+        // token exists in the database? :
+        const resetToken = await getResetToken(token);
+        if (!resetToken) {
+            return res.status(404).json(
+                {
+                    success: false,
+                    message: "Token Not Found",
+                }
+            );
+        };
+
+        // token expired? :
+        if (new Date() > new Date(resetToken.expires_at)) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: " Reset token has expired."
+                }
+            );
+        };
+
+        //token used? :
+        if (resetToken.used) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: " Reset token already used."
+                }
+            );
+        };
+
+        // All checks passed! Attach token data to request
+        req.resetTokenValidation = resetToken;
+
+        next();
+
+    } catch (error) {
+        console.error("Token reset validation error", error.message);
+        res.status(500).json(
+            {
+                success: false,
+                message: "Internal server error during token validation"
+            }
+        );
+    };
+};
--- src/Models/resetPassword.model.js
+++ src/Models/resetPassword.model.js
@@ -6,8 +6,8 @@ import database from "../Config/database.js";
 export const storeResetToken = async (userId, hashedToken, expiresAt) => {
     try {
 
-        const { result } = await database.pool.query(
-            "INSERT INTO  password_reset_tokens (user_id, token, expires_at) VALUES(?, ?, ?)",
+        const [result] = await database.pool.query(
+            'INSERT INTO  password_reset_tokens (user_id, token, expires_at) VALUES(?, ?, ?)',
             [userId, hashedToken, expiresAt]
         );
         return result.insertId;
@@ -18,15 +18,49 @@ export const storeResetToken = async (userId, hashedToken, expiresAt) => {
     }
 };
 
-export const getResetToken = async () => {
+export const getResetToken = async (hashedToken) => {
     try {
-        const [result] = await database.pool.query(
-            'SELECT * FROM password_reset_tokens'
-        )
+        const [row] = await database.pool.query('SELECT * FROM password_reset_tokens WHERE token = ?', [hashedToken]);
+        return row[0];
 
     } catch (error) {
-        console.error('Error occured while getting the reset token');
+        console.error('Error occurred while getting the reset token');
+        throw error;
+
+    }
+};
+
+export const markTokenAsUsed = async (tokenId) => {
+    try {
+        const [result] = await database.pool.query('UPDATE password_reset_tokens SET used = true Where id = ?', [tokenId]);
+        return result.affectedRows;
+
+    } catch (error) {
+        console.error('Error occurred while marked the token as used');
+        throw error;
+    }
+};
+
+export const deleteResetToken = async (tokenId) => {
+    try {
+        const [result] = await database.pool.query('DELETE FROM password_reset_tokens WHERE id = ?', [tokenId]);
+        return result.affectedRows;
+
+    } catch (error) {
+        console.error('Error occurred while deleting reset tokens');
         throw error;
 
     }
 }
+export const deleteAllExpiredToken = async () => {
+    try {
+        const [result] = await database.pool.query('DELETE FROM password_reset_tokens WHERE expires_at < NOW()');
+        return result.affectedRows;
+
+    } catch (error) {
+        console.error('Error occurred while deleting expired tokens');
+        throw error;
+    };
+};
+
+
